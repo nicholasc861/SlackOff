a69d76427784c5f3fe26f0298ce0e08d9b80d4c8
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2019-03-24 12:15:39 -0400

    fix and clean up

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var userName
 var message
 var outgoingPhone
 
+
+
 app.post('/', urlencodedParser, function (req, res) {
   var accountSid = 'AC069753dcf0568bc05d491ddb5dd4b5c4';
   var authToken = '24b26bcadf2fbbb198593134e0572da9';
@@ -19,46 +21,39 @@ app.post('/', urlencodedParser, function (req, res) {
 
   globalVar = req.body.text;
   userName = req.body.user_name;
-  message = globalVar.substr(13,);
-  outgoingPhone = globalVar.substr(0, 12);
 
+ 
+  outgoingPhone = globalVar.substr(0, 8);
 
-  if outgoingPhone == 'everyone'{
-    outgoingPhone = ['+14165647680', '+14166028809']
+  if(outgoingPhone == 'everyone'){
+    outgoingPhone = ['+14165647680', '+14166028809', '+16472997733']
     var arrayLength = outgoingPhone.length;
+    message = globalVar.substr(9,);
 
     for (var i = 0; i < arrayLength; i++){
        client.messages.create({
         body: 'From ' + userName + ': ' + message,
         from: '+16473608227',
-        to: outgoingPhone
-        })
-      .then((message) => console.log(message.sid))
-      .catch((message) => console.log(message))
-       console.log(globalVar);
-       console.log(userName)
+        to: outgoingPhone[i]
 
-      })
-    }
+        })
+      }
+
   } else {
     outgoingPhone = '+1' + globalVar.substr(0, 11);
-
+    message = globalVar.substr(11,);
 
     client.messages.create({
     body: 'From ' + userName + ': ' + message,
     from: '+16473608227',
     to: outgoingPhone
+
     })
-    .then((message) => console.log(message.sid))
-    .catch((message) => console.log(message))
-   
-    console.log(globalVar);
-    console.log(userName)
 
-
-
+  }
+  res.send('Successfully sent ' + message + ' to ' + outgoingPhone);
 })
-res.send('Successfully sent ' + message + ' to ' + outgoingPhone);
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
