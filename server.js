de54c204c323a6dcdf3ed258eea50cd4bd9e1ee6
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2019-03-24 10:43:50 -0400

    working copy of SlackOff

---
+++ server.js
@@ -0,0 +1,40 @@
+const twilio = require('twilio')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var globalVar
+var userName
+var message
+var outgoingPhone
+
+app.post('/', urlencodedParser, function (req, res) {
+  globalVar = req.body.text;
+  userName = req.body.user_name;
+  outgoingPhone = globalVar.substr(0, 12);
+  message = globalVar.substr(13,);
+  res.send('Successfully sent ' + message + ' to ' + outgoingPhone);
+  console.log(globalVar);
+  console.log(userName)
+  
+    var accountSid = 'AC069753dcf0568bc05d491ddb5dd4b5c4';
+    var authToken = '24b26bcadf2fbbb198593134e0572da9';
+    var client = require('twilio')(accountSid, authToken);
+
+
+    client.messages.create({
+    body: 'From ' + userName + ' ' + message,
+    from: '+16473608227',
+    to: outgoingPhone
+    })
+    .then((message) => console.log(message.sid))
+    .catch((message) => console.log(message))
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
